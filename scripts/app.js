e396de6ba625db828f6a253e8cb2d3aec27d1736
ogonz801 <g.osvaldo96@gmail.com>
2022-05-05 22:21:25 -0600

    finished project

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 const startBtn = document.querySelector('#startBtn')
+const getReady = document.querySelector('.getReady')
 const hideStart = document.querySelector('.start-button')
 const restartBtn = document.querySelector('#restart')
+const results = document.querySelector('.results')
+const resultDisplay = document.querySelector('#result-display2')
 const timer = document.querySelector('#timer h2')
 const score = document.querySelector('#score h2')
 const holes = document.querySelectorAll('.holes')
@@ -8,22 +11,24 @@ const moles = document.querySelectorAll('.moles')
 const hiddenClasses = document.querySelector('.hide')
 
 let lastHole
+let moleHit = 0
 let gameOver = false
-let gameTimer = 30
 let scoreCount = 0
+let gameTimer = 15
+let prepTimer = 3
 
 // starts game when button clicked
 let gameStart = () => {
     hideStart.classList.add('hide')
-    hiddenClasses.classList.remove('hide')
-    
+
     // delays the game from starting immediately after button press
-    let prepTimer = 2
     const countDown = setInterval(() => {
     prepTimer--
+    getReady.textContent = prepTimer
     if (prepTimer <= 0) {
         clearInterval(countDown)
         hiddenClasses.classList.remove('hide')
+        getReady.classList.add('hide')
         
         // starts the game timer
         let gameTime = setInterval(() => {
@@ -32,10 +37,11 @@ let gameStart = () => {
     if (gameTimer <= 0) {
         clearInterval(gameTime)
         gameOver = true
+        displayResults()
     }
         },1000)
-       
-        // timer for the game to end
+
+    // stops the moles from continuing to peep out
         setTimeout (() => {
         peep()
         }, gameTimer)
@@ -43,6 +49,14 @@ let gameStart = () => {
 }, 1000)
 }
 
+// displays results once game has ended
+let displayResults = () => {
+        if (gameOver === true) {
+        hiddenClasses.classList.add('hide')
+        results.classList.remove('hide2')
+        resultDisplay.textContent = `You bonked a total of ${moleHit} moles. Score: ${scoreCount}.`
+    }
+}
 
 // randomly chooses a hole for the mole
 let randomHole = () => {
@@ -64,12 +78,14 @@ let peep = () => {
     setTimeout(() => {
         hole.classList.remove('peep')
         if (!gameOver) peep()
-    }, Math.random() * 300 + 800)
+    }, Math.random() * 400 + 700)
 }
+
 
 // bonk the mole to earn points
 function bonk(e) {
-    scoreCount++
+    moleHit++
+    scoreCount +=2
     this.style.backgroundImage = 'url("img/moleHit.png")'
     this.style.pointerEvents = 'none'
     setTimeout(() => {
@@ -79,5 +95,22 @@ function bonk(e) {
     score.textContent = scoreCount
 }
 
+// resets and restarts game
+function restart() {
+    getReady.classList.remove('hide')
+    results.classList.add('hide2')
+    gameTimer.textContent = '30'
+    scoreCount.textContent = '0'
+    lastHole
+    gameOver = false
+    getReady.textContent = 3
+    prepTimer = 3
+    gameTimer = 15
+    scoreCount = 0
+    moleHit = 0
+    gameStart()
+}
+
 moles.forEach(mole => mole.addEventListener('click', bonk))
 startBtn.addEventListener('click', gameStart)
+restartBtn.addEventListener('click', restart)
